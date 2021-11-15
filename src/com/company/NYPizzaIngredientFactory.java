8f10a0c07fbe1545746480daa87cf446d7d1ed17
Dmitriy Antonov <79038945169@yandex.ru>
2021-11-15 16:29:38 +0300

    3

---
+++ src/com/company/NYPizzaIngredientFactory.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class NYPizzaIngredientFactory implements PizzaIngredienFactory{
+    public Dough createDough() {
+        return new ThinCrustDough();
+    }
+    public Sauce createSauce() {
+        return new MarinaraSauce();
+    }
+
+    public Cheese createCheese() {
+        return new ReggianoCheese();
+    }
+
+    public Veggies[] createVeggies() {
+        Veggies veggies[] = { new Garlic(), new Onion(), new Mushroom(), new RedPepper() };
+        return veggies;
+    }
+    public Pepperoni createPepperoni() {
+        return new SlicedPepperoni();
+    }
+    public Clams createClam() {
+        return new FreshClams();
+    }
+
+}
---
+++ src/com/company/PizzaIngredienFactory.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public interface PizzaIngredienFactory {
+    public Dough createDough();
+    public Sauce createSauce();
+    public Cheese createCheese();
+    public Veggies[] createVeggies();
+    public Pepperoni createPepperoni();
+    public Clams createClam();
+
+}
